a0a7f16b0971c80f69167f6778a2b39eea562608
Adeimantius <hidden.identity@gmail.com>
2016-05-25 20:29:28 -0700

    Added sphere intersect calculation

--- rayTracer/rayTracer/main.cpp
+++ rayTracer/rayTracer/main.cpp
@@ -9,10 +9,23 @@
 #include <iostream>
 #include "ray.h"
 #include "vec3.h"
-
+bool hitSphere (const vec3& center, float radius, const ray &r ) {
+    vec3 oc = r.origin() - center;
+    float a = r.direction().dot(r.direction());
+    float b = 2 * r.direction().dot(oc);
+    float c = oc.dot(oc) - (radius * radius);
+    float discriminant = (b * b) - (4 * a * c);
+    // if discriminant is > 0, the ray intersects the sphere twice
+    // if discriminant is == 0, the ray is tangent to the sphere
+    // if discriminant is < 0, the ray does not intersect the sphere
+    return (discriminant > 0);
+};
 vec3 color(const ray &r){
+    if(hitSphere(vec3(0,0,-1), 0.5, r)){
+        return vec3(1.0, 0.0, 0.0);
+    }
     vec3 unitDirection = unitVector(r.direction());
-    float t = 0.5 * unitDirection.y() + 1.0;
+    float t = 0.5 * unitDirection.y + 1.0;
     return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
@@ -22,13 +35,21 @@ int main(int argc, const char * argv[]) {
     int ny = 400;
     
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    vec3 lowerLeft(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
     
     for (int j = ny-1; j > 0; j--){
         for (int i = 0; i < nx; i++){
-            vec3 col ((float)i / (float)nx, (float)j / (float)ny, (float)i / (float)j);
-            int ir = (int)255.99*col.r();
-            int ig = (int)255.99*col.g();
-            int ib = (int)255.99*col.b();
+            float u = (float)i / (float)nx;
+            float v = (float)j / (float)ny;
+            ray r(origin, lowerLeft + u * horizontal + v * vertical);
+            
+            vec3 col = color(r);
+            int ir = (int)255.99*col.r;
+            int ig = (int)255.99*col.g;
+            int ib = (int)255.99*col.b;
             
             int tr = ir % 256;
             int tg = ig % 256;
@@ -39,6 +60,22 @@ int main(int argc, const char * argv[]) {
         }
     };
     
-    
     return 0;
 }
+
+/// Pattern 1, fan of rainbows
+//for (int j = ny-1; j > 0; j--){
+//    for (int i = 0; i < nx; i++){
+//        vec3 col ((float)i / (float)nx, (float)j / (float)ny, (float)i / (float)j);
+//        int ir = (int)255.99*col.r;
+//        int ig = (int)255.99*col.g;
+//        int ib = (int)255.99*col.b;
+//        
+//        int tr = ir % 256;
+//        int tg = ig % 256;
+//        int tb = ib % 256;
+//        
+//        std::cout << tr << " " << tg << " " << tb << "\n";
+//        
+//        }
+//    };
--- rayTracer/rayTracer/vec3.h
+++ rayTracer/rayTracer/vec3.h
@@ -1,41 +1,167 @@
 #ifndef __VEC3__
 #define __VEC3__
 
+// Credit to in0x [https://github.com/in0x/Tracer]
 #include <math.h>
 #include <stdlib.h>
 #include <iostream>
 
-class vec3 {
+class vec3
+{
 public:
-    vec3 () {}
-    vec3(float e00, float e01, float e02){ e[0] = e00; e[1] = e01; e[2] = e02; }
-    inline float x() const { return e[0]; }
-    inline float y() const { return e[1]; }
-    inline float z() const { return e[2]; }
-    inline float r() const { return e[0]; }
-    inline float g() const { return e[1]; }
-    inline float b() const { return e[2]; }
+    vec3() {}
+    vec3( float _x, float _y, float _z ) : x( _x ), y( _y ), z( _z ),
+                                           r( _x ), g( _y ), b( _z )  {}
     
-    inline const vec3& operator+() const {return *this; };
-    inline vec3 operator-() const {return vec3(-e[0], -e[1], -e[2]); };
-    inline float operator[](int i) const {return e[i]; };
-    inline float& operator[](int i) {return e[i]; };
-
-    inline vec3& operator +=(const vec3 &v2);
-    inline vec3& operator -=(const vec3 &v2);
-    inline vec3& operator *=(const vec3 &v2);
-    inline vec3& operator /=(const vec3 &v2);
-    inline vec3& operator *=(const float t);
-    inline vec3& operator /=(const float t);
-
-    inline float length() const {
-        return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]); }
-    inline float squaredlength() const {
-        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2]; }
-    inline void makeUnitVector();
-
-    float e[3];
+    // Public Member Functions
+    float x;
+    float y;
+    float z;
+    float r;
+    float g;
+    float b;
     
+    vec3 operator-() const { return vec3( -x, -y, -z ); };
     
+    bool operator==( const vec3& rhs )
+    {
+        return x == rhs.x && y == rhs.y && z == rhs.z;
+    }
+    
+    vec3& operator+=( const vec3& rhs )
+    {
+        x += rhs.x;
+        y += rhs.y;
+        z += rhs.z;
+        return *this;
+    }
+    
+    vec3& operator-=( const vec3& rhs )
+    {
+        x -= rhs.x;
+        y -= rhs.y;
+        z -= rhs.z;
+        return *this;
+    }
+    
+    vec3& operator*=( const vec3& rhs )
+    {
+        x *= rhs.x;
+        y *= rhs.y;
+        z *= rhs.z;
+        return *this;
+    }
+    
+    vec3& operator/=( const vec3& rhs )
+    {
+        x /= rhs.x;
+        y /= rhs.y;
+        z /= rhs.z;
+        return *this;
+    }
+    
+    vec3& operator*=( const float t )
+    {
+        x *= t;
+        y *= t;
+        z *= t;
+        return *this;
+    }
+    
+    vec3& operator/=( const float t )
+    {
+        x /= t;
+        y /= t;
+        z /= t;
+        return *this;
+    }
+    
+    float length() const
+    {
+        return sqrt( x*x + y*y + z*z );
+    }
+    
+    float lengthsquared() const
+    {
+        return x*x + y*y + z*z;
+    }
+    
+    void normalize()
+    {
+        float len = 1 / length();
+        x *= len; y *= len; z *= len;
+    }
+    
+    float dot( const vec3& rhs ) const
+    {
+        return x * rhs.x + y * rhs.y + z * rhs.z;
+    }
+    
+    vec3 cross( const vec3 rhs ) const
+    {
+        return vec3(
+                    y * rhs.z - z * rhs.y,
+                    -( x * rhs.z - z * rhs.x ),
+                    x * rhs.y - y * rhs.x
+                    );
+    }
+    
+    vec3 unitVector() const;
 };
+
+inline std::ostream& operator<<( std::ostream& os, const vec3& vec )
+{
+    os << vec.x << " " << vec.y << " " << vec.z;
+    return os;
+}
+
+inline vec3 operator+( const vec3& lhs, const vec3& rhs )
+{
+    return vec3( lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z );
+}
+
+inline vec3 operator-( const vec3& lhs, const vec3& rhs )
+{
+    return vec3( lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z );
+}
+
+inline vec3 operator*( const vec3& lhs, const vec3& rhs )
+{
+    return vec3( lhs.x * rhs.x, lhs.y * rhs.y, lhs.z * rhs.z );
+}
+
+inline vec3 operator/( const vec3& lhs, const vec3& rhs )
+{
+    return vec3( lhs.x / rhs.x, lhs.y / rhs.y, lhs.z / rhs.z );
+}
+
+inline vec3 operator*( const vec3& lhs, float rhs )
+{
+    return vec3( lhs.x * rhs, lhs.y * rhs, lhs.z * rhs );
+}
+
+inline vec3 operator/( const vec3& lhs, float rhs )
+{
+    return vec3( lhs.x / rhs, lhs.y / rhs, lhs.z / rhs );
+}
+
+inline vec3 operator*( float lhs, const vec3& rhs )
+{
+    return vec3( lhs * rhs.x, lhs * rhs.y, lhs * rhs.z );
+}
+
+inline vec3 operator/( float lhs, const vec3& rhs )
+{
+    return vec3( lhs / rhs.x, lhs / rhs.y, lhs / rhs.z );
+}
+
+inline vec3 reflect(const vec3& v, const vec3& n)
+{
+    return v - 2 * v.dot(n) * n;
+}
+//
+inline vec3 unitVector(vec3 v)
+{
+    return v / v.length();
+}
 #endif
